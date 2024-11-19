7aff8289529b9de2d5434c5f6c13b9552334f5bc
8001Aditya <aditya8001@gmail.com>
2024-11-19 12:54:27 +0530

    second version of lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -13,6 +13,31 @@ def lambda_handler(event, context):
         object_key = event['Records'][0]['s3']['object']['key']
 
         print(f"Processing file: {object_key} from bucket: {bucket_name}")
+
+        response = s3.get_object(Bucket=bucket_name, key=object_key)
+        image_data = response['Body'].read()
+
+        image = Image.open(io.BytesIO(image_data))
+
+        resized_image = image.resize((300,300))
+
+        buffer =  io.BytesIO()
+        resized_image.save(buffer, format=image.format)
+        buffer.seek(0)
+
+        processed_key = f"processed/{object_key}"
+        s3.put_object(Bucket=PROCESSED_BUCKET, key=processed_key, Body=buffer)
+
+        print(f"Processed image saved to: {PROCESSED_BUCKET}/{processed_key}")
+
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('ImageMetadata')
+        table.put_item(item={
+            'ImageID' : object_key,
+            'ProcessedURL' : f"s3://{PROCESSED_BUCKET}/{processed_key}",
+            'Size': f"{resized_image.size[0]}x{resized_image.size[1]}",
+            'Timestamp': response['LastModified'].strftime('%Y-%m-%d %H:%M:%S')
+        })
         
     except Exception as e:
         print(f"Error processing file: {e}")
